6ea1b36d67854c60e8188f94a7f1728cefa02948
deepikaarcot18-png <deepikaarcot18@gmail.com>
2026-06-11 15:55:14 +0530

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:' https://deepikaarcot18-png.github.io/bmsfrontend6/',
 })
